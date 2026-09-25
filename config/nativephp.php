b763ee52f80f796fd0963607085b767dda98827c
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2026-09-26 01:17:12 +0300

    chore: bump version to v1.3.1

--- config/nativephp.php
+++ config/nativephp.php
@@ -8,7 +8,7 @@ return [
      * It is used to determine if the app needs to be updated.
      * Increment this value every time you release a new version of your app.
      */
-    'version' => env('NATIVEPHP_APP_VERSION', '1.3.0'),
+    'version' => env('NATIVEPHP_APP_VERSION', '1.3.1'),
 
     /**
      * The ID of your application. This should be a unique identifier
